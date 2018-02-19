953ebc0a823d1d60e84fede6407892038299585f
cowboycod3r <cowboycod3r@gna13.de>
2018-02-19 09:55:01 +0100

    added install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+pwd=$(pwd)
+
+echo "origin: $pwd"
+
+echo -n 'Creating symbolic links...'
+
+cd ~
+
+mv .vim .vim.legacy
+mv .vimrc .vimrc.legacy
+
+ln -s $pwd/vim .vim
+ln -s $pwd/vimrc .vimrc
+
+echo ' done.'
+ls -la .vim*
+
+cd $pwd
